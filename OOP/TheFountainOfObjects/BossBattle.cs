d61523e526e49a327d03c999d55977f47858f8a2
clintonbampoe <clintbamp@gmail.com>
2025-12-19 14:05:42 +0000

    L31-Boss Battle: Expansions--Fountain Of Objects. Complete

--- OOP/TheFountainOfObjects/BossBattle.cs
+++ OOP/TheFountainOfObjects/BossBattle.cs
@@ -1,32 +1,43 @@
 ﻿
-
-using System.Linq.Expressions;
-
 class BossBattle
 {
     public static void Run()
     {
-        Board game = new Board();
+        BoardSize size = SetBoardSize();
+
+        Board game = new(size);
+        Move move = new(size);
+
         game.InitializeGame();
+        game.Grid = Pit.SetNew(game.Grid, size);
         do
         {
             game.DisplayGameState();
-            try
+            if (game.GameOver)
+                break;
+
+            else
             {
-                game.Command();
-            }
-            catch (IndexOutOfRangeException ex)
-            {
-                Message.PrintErrorMessage(ex.Message);
-            }
-            catch (ArgumentException ex)
-            {
-                Message.PrintErrorMessage(ex.Message);
-            }
-            game.CheckGameOver();
+                try
+                {
+                    game.Command(move);
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    Message.PrintErrorMessage(ex.Message);
+                }
+                catch (ArgumentException ex)
+                {
+                    Message.PrintErrorMessage(ex.Message);
+                }
+            } 
             
         } while (!game.GameOver);
-        Console.WriteLine("You win!");
+
+        if (game.GameWon)
+            Console.WriteLine("You win");
+        else
+            Message.PrintErrorMessage("You lost!"); // not an error, just wanted the red text color
 
     }
 
@@ -34,18 +45,38 @@ class BossBattle
     class Board
     {
         // Constructors
-        public Board()
+        public Board(BoardSize size)
         {
-            Grid = new GridState[4, 4];
+            if (size == BoardSize.Small)
+            {
+                GridSize = 4;
+                BoardSizeType = BoardSize.Small;
+            }
+            else if (size == BoardSize.Medium)
+            {
+                GridSize = 6;
+                BoardSizeType = BoardSize.Medium;
+            }
+            else
+            {
+                GridSize = 8;
+                BoardSizeType = BoardSize.Large;
+            }
+
+            Grid = new GridState[GridSize, GridSize];
         }
+
 
 
         // properties
         public GridState[,] Grid { get; set; }
+        public int GridSize { get; init; }
+        public BoardSize BoardSizeType { get; init; }
         public int Row { get; private set; }
         public int Col { get; private set; }
         public bool ActivateFountain { get; private set; }
-        public bool GameOver { get; private set; }
+        public bool GameWon { get; private set; } = false;
+        public bool GameOver { get; set; }
 
 
 
@@ -54,7 +85,7 @@ class BossBattle
         public void InitializeGame()
         {
             SetStartingLocation();
-            SetFountainLocation(0, 2);
+            SetFountainLocation(3, 3);
         }
         public void DisplayGameState()
         {
@@ -70,15 +101,16 @@ class BossBattle
         }
 
         // Init Game Method Cluster
+
         void SetFountainLocation(int row, int col)
         {
-            Grid[row, col] = GridState.F;
+            Grid[row, col] = GridState.Fountain;
         }
         void SetStartingLocation()
         {
             Row = 0;
             Col = 0;
-            Grid[Row, Col] = GridState.X;
+            Grid[Row, Col] = GridState.Current;
         }
 
         // Game State Method Cluster
@@ -90,21 +122,37 @@ class BossBattle
                 if (ActivateFountain)
                 {
                     Message.PrintFountainMessage("Fountain of objects has been reactivated, and you escaped with your life");
+                    GameWon = true;
+                    GameOver = true;
                     return;
                 }
 
-                Message.PrintEntranceMessage("You see light in this room coming from outside the cavern. This is the entrance.");
+                else Message.PrintEntranceMessage("You see light in this room coming from outside the cavern. This is the entrance.");
             }
-            if (Grid[Row, Col] == GridState.F)
+
+            if (Grid[Row, Col] == GridState.ActiveFountain)
             {
                 if (ActivateFountain)
                 {
                     Message.PrintFountainMessage("You hear the rushing waters of the fountain of objects. It has been reactivated.");
-                    return;
                 }
 
-                Message.PrintFountainMessage("You hear water dripping in this room. The fountain of objects is here.");
+                else Message.PrintFountainMessage("You hear water dripping in this room. The fountain of objects is here.");
             }
+            else if (Grid[Row, Col] == GridState.ActivePit)
+            {
+                Message.PrintPitMessage("You fell into a Pit");
+                GameOver = true;
+                return;
+            }
+
+            if (Pit.IsCloseBy(Grid, Row, Col))
+            {
+                Message.PrintPitMessage("You feel a draft. There is a pit in a nearby room. ");
+                return;
+            }
+            
+            
 
             Console.WriteLine("Everything is dark");
 
@@ -113,17 +161,30 @@ class BossBattle
         {
             Console.WriteLine($"You are in the room at (Row={Row} Col={Col})");
         }
+       
         void DisplayGrid(GridState[,] Grid)
         {
-            string line = "+---+---+---+---+";
+            string line;
+            if (BoardSizeType == BoardSize.Small)
+                line = "+---+---+---+---+";
+            else if (BoardSizeType == BoardSize.Medium)
+                line = "+---+---+---+---+---+---+";
+            else
+                line = "+---+---+---+---+---+---+---+---+";
 
-            for (int col = 0; col < 4; col++)
+            for (int col = 0; col < GridSize; col++)
             {
                 Console.WriteLine(line);
-                for (int row = 0; row < 4; row++)
+                for (int row = 0; row < GridSize; row++)
                 {
-                    
-                    string state = (row == Row && col == Col) ? "X" : " ";
+                    string state = " ";
+                    if (Grid[row, col] == GridState.Current || Grid[row, col] == GridState.ActiveFountain || Grid[row, col] == GridState.ActivePit)
+                    {
+                        state = "X";
+                        Row = row;
+                        Col = col;
+                    }
+                        
 
                     Console.Write("| ");
                     Console.ForegroundColor = ConsoleColor.Green;
@@ -136,98 +197,20 @@ class BossBattle
             Console.WriteLine(line);
         }
 
-        public void CheckGameOver()
-        {
-            if (ActivateFountain)
-            {
-                if (Row == 0 && Col == 0)
-                {
-                    GameOver = true;
-                    return;
-                }
-            }
-        }
 
         // Commands Method Cluster
         bool EnableFountain()
         {
-            if (Grid[Row, Col] == GridState.F)
+            if (Grid[Row, Col] == GridState.ActiveFountain)
             {
                 ActivateFountain = true;
                 return true;
             }
             return false;
         }
-        public bool MoveNorth()
-        {
-            if (Col - 1 < 0)
-            {
-                return false;
-            }
-
-            Grid[Row, Col] = GridState.O;
-            if (Grid[Row, Col - 1] == GridState.F)
-            {
-                Col--;
-                return true;
-            }
-
-            Grid[Row, --Col] = GridState.X;
-            return true;
-        }
-        public bool MoveSouth()
-        {
-            if (Col + 1 > 4)
-            {
-                return false;
-            }
-
-            Grid[Row, Col] = GridState.O;
-            if (Grid[Row, Col + 1] == GridState.F)
-            {
-                Col++;
-                return true;
-            }
-
-            Grid[Row, ++Col] = GridState.X;
-            return true;
-        }
-        public bool MoveEast()
-        {
-            if (Row + 1 > 4)
-            {
-                return false;
-            }
-
-            Grid[Row, Col] = GridState.O;
-            if (Grid[Row + 1, Col] == GridState.F)
-            {
-                Row++;
-                return true;
-            }
-
-            Grid[++Row, Col] = GridState.X;
-            return true;
-        }
-        public bool MoveWest()
-        {
-            if (Row - 1 < 0)
-            {
-                return false;
-            }
-
-            Grid[Row, Col] = GridState.O;
-            if (Grid[Row - 1, Col] == GridState.F)
-            {
-                Row--;
-                return true;
-            }
-
-            Grid[--Row, Col] = GridState.X;
-            return true;
-        }
+        
         // Input
-        public void Command()
+        public void Command(Move Move)
         {
             Console.Write("What do you want do? ");
             string input = TakeInput();
@@ -244,28 +227,28 @@ class BossBattle
             }
             else if (input.ToLower() == "move north")
             {
-                if (MoveNorth())
+                if (Move.North(Grid, Row, Col))
                     return;
                 else
                     throw new IndexOutOfRangeException(OutOfBoundsErrorMessage);
             }
             else if (input.ToLower() == "move south")
             {
-                if (MoveSouth())
+                if (Move.South(Grid, Row, Col))
                     return;
                 else
                     throw new IndexOutOfRangeException(OutOfBoundsErrorMessage);
             }
             else if (input.ToLower() == "move east")
             {
-                if (MoveEast())
+                if (Move.East(Grid, Row, Col))
                     return;
                 else
                     throw new IndexOutOfRangeException(OutOfBoundsErrorMessage);
             }
             else if (input.ToLower() == "move west")
             {
-                if (MoveWest())
+                if (Move.West(Grid, Row, Col))
                     return;
                 else
                     throw new IndexOutOfRangeException(OutOfBoundsErrorMessage);
@@ -276,6 +259,105 @@ class BossBattle
             }
         }
 
+    }
+
+    class Move
+    {
+        // Constructors
+        public Move(BoardSize size)
+        {
+            if (size == BoardSize.Small)
+                GridSize = 4;
+            else if (size == BoardSize.Medium)
+                GridSize = 6;
+            else
+                GridSize = 8;
+        }
+
+        // Properties
+        public int GridSize { get; init; }
+
+        // Methods
+        public bool North(GridState[,] grid, int x, int y)
+        {
+            int dx = 0;
+            int dy = -1;
+
+            return MoveLogic(grid, x, y, dx, dy);
+
+        }
+
+        public bool South(GridState[,] grid, int x, int y)
+        {
+            int dx = 0;
+            int dy = +1;
+
+            return MoveLogic(grid, x, y, dx, dy);
+
+        }
+        public bool East(GridState[,] grid, int x, int y)
+        {
+            int dx = +1;
+            int dy = 0;
+
+            return MoveLogic(grid, x, y, dx, dy);
+            
+        }
+        public bool West(GridState[,] grid, int x, int y)
+        {
+            int dx = -1;
+            int dy = 0;
+
+            return MoveLogic(grid, x, y, dx, dy);
+            
+        }
+
+        
+
+        bool MoveLogic(GridState[,] grid, int x, int y, int dx, int dy)
+        {
+
+            // check if move is OutOfBounds
+            if (x + dx < 0 || x + dx >= GridSize || y + dy < 0 || y + dy >= GridSize)
+            {
+                return false;
+            }
+
+
+            // if current cell is a pit
+            if (grid[x, y] == GridState.ActivePit)
+                grid[x, y] = GridState.Pit;
+            else if (grid[x, y] == GridState.Fountain || grid[x, y] == GridState.ActiveFountain)
+                grid[x, y] = GridState.Fountain;
+            else grid[x, y] = GridState.Empty;
+
+
+            // Check if move is FountainLocation
+            if (grid[x + dx, y + dy] == GridState.Fountain)
+            {
+                grid[x + dx, y + dy] = GridState.ActiveFountain;
+                return true;
+            }
+
+
+            // Check if move results in Pit
+            if (grid[x + dx, y + dy] == GridState.Pit)
+            {
+                grid[x + dx, y + dy] = GridState.ActivePit;
+                return true;
+            }
+
+            grid[x + dx, y + dy] = GridState.Current;
+            return true;
+        }
+
+        bool IsActivePit(GridState grid)
+        {
+            if (grid == GridState.ActivePit)
+                return true;
+
+            return false;
+        }
     }
 
     class Message
@@ -306,30 +388,190 @@ class BossBattle
             Console.WriteLine($"{message}");
             Console.ForegroundColor = ConsoleColor.White;
         }
+        public static void PrintPitMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{message}");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
     }
 
-
-    // Methods
-    static bool IsFountain(GridState[,] Grid, int row, int col)
+    class Pit()
     {
-        if (Grid[row, col] == GridState.F)
+        // Constructor
+        public static GridState[,] SetNew(GridState[,] grid, BoardSize size)
         {
-            return true;
+            Random rnd = new Random(DateTime.Now.Second);
+
+            if(size == BoardSize.Small)
+            {
+                GridBoundary = 4;
+                for(int i = 0; i < 3; i++)
+                {
+                    int XPit1 = rnd.Next(0, 4);
+                    int YPit1 = rnd.Next(0, 4);
+
+                    if (grid[XPit1, YPit1] == GridState.Fountain)
+                        i--;
+                    else if (XPit1 == 0 && YPit1 == 0)
+                        i--;
+                    else
+                    {
+                        grid[XPit1, YPit1] = GridState.Pit;
+                    }
+
+                }
+                
+            }
+
+            else if(size == BoardSize.Medium)
+            {
+                GridBoundary = 6;
+                for(int i = 0; i < 5; i++)
+                {
+                    int XPit1 = rnd.Next(0, 6);
+                    int YPit1 = rnd.Next(0, 6);
+
+                    if (grid[XPit1, YPit1] == GridState.Fountain)
+                        i--;
+                    else if (XPit1 == 0 && YPit1 == 0)
+                        i--;
+                    else
+                    {
+                        grid[XPit1, YPit1] = GridState.Pit;
+                    }
+
+                }
+                
+            }
+
+            else if(size == BoardSize.Large)
+            {
+                GridBoundary = 8;
+                for(int i = 0; i < 10; i++)
+                {
+                    int XPit1 = rnd.Next(0, 8);
+                    int YPit1 = rnd.Next(0, 8);
+
+                    if (grid[XPit1, YPit1] == GridState.Fountain)
+                        i--;
+                    else if (XPit1 == 0 && YPit1 == 0)
+                        i--;
+                    else
+                    {
+                        grid[XPit1, YPit1] = GridState.Pit;
+                    }
+
+                }
+                
+            }
+
+            return grid;
         }
 
-        return false;
+        // Properties
+        public static int GridBoundary { get; private set; }
+        public static bool IsCloseBy(GridState[,] grid, int x, int y)
+        {
+            bool isClose = false;
+
+            // all four cardinal points
+            if(!(y - 1 < 0))
+            {
+                if (North(grid, x, y))
+                    isClose = true;
+            }
+                
+            if (!(y + 1 >= GridBoundary))
+            {
+                if(South(grid, x, y))
+                    isClose = true;
+            }
+            if (!(x - 1 < 0))
+            {
+                if (West(grid, x, y))
+                    isClose = true;
+            }
+            if (!(x + 1 >= GridBoundary))
+            {
+                if (East(grid, x, y))
+                    isClose = true;
+            }
+
+            return isClose;
+
+            // MINI METHODS
+            bool North(GridState[,] grid, int x, int y)
+            {
+                int dy = -1;
+                return (grid[x, y + dy] == GridState.Pit);
+            }
+
+            bool South(GridState[,] grid, int x, int y)
+            {
+                int dy = +1;
+                return (grid[x, y + dy] == GridState.Pit);
+            }
+
+            bool East(GridState[,] grid, int x, int y )
+            {
+                int dx = +1;
+                return (grid[x + dx, y] == GridState.Pit);
+            }
+            
+            bool West(GridState[,] grid, int x, int y)
+            {
+                int dx = -1;
+                return (grid[x + dx, y] == GridState.Pit);
+            }
+        }
     }
+
 
     // Helper Methods
     static string TakeInput()
     {
         Console.ForegroundColor = ConsoleColor.Green;
-        string? input = Console.ReadLine();
+        string input = Console.ReadLine() ?? "";
         Console.ForegroundColor = ConsoleColor.White;
         return input;
     }
-    
+    static BoardSize SetBoardSize()
+    {
+        Console.WriteLine("Choose board size");
+        Console.WriteLine("1. Small (4 * 4)");
+        Console.WriteLine("2. Medium (6 * 6)");
+        Console.WriteLine("3. Large (8 * 8)");
+
+        do
+        {
+            Console.Write($"Input> ");
+            string input = TakeInput();
+
+            switch (input.ToLower())
+            {
+                case "1":
+                case "small":
+                    return BoardSize.Small;
+
+                case "2":
+                case "medium":
+                    return BoardSize.Medium;
+
+                case "3":
+                case "large":
+                    return BoardSize.Large;
+
+                default:
+                    Console.WriteLine("Invalid Input! Try Again!");
+                    break;
+            }
+
+        } while (true);
+
+    }
 
     // Enums
-    enum GridState { O, X, F }
+    enum GridState { Empty, Current, Fountain, Pit, ActiveFountain, ActivePit }
+    enum BoardSize { Small, Medium, Large}
 }
